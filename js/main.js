083e1914cb9dfc5f34dfa61cce260398d55d5c9d
Marta <martaguilar.soto@gmail.com>
2018-05-14 14:54:08 +0200

    create examples of console.

---
+++ js/main.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const dogs = [
+  {
+    name: 'Snikers',
+    age: 2
+  },
+  {
+    name: 'hugo',
+    age: 8
+  }
+]
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+//Regular
+console.log('Hello');
+
+//Interpolated
+console.log('hello', 'Hi');
+
+//Styled
+console.log('%c I am some great text', 'font-size:50px; background:red; text-shadow:10px 10px 0 blue')
+
+//Warning
+console.warn('oh no');
+
+//Error
+console.error('shit');
+
+//Info
+console.info('Crocodiles eat 3-4 people per year');
+
+//Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'that is wrong!');
+
+//Clearing
+console.clear();
+
+//Viewing DOM Elements
+console.log(p);
+console.dir(p);
+console.clear();
+
+//Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
+
+//Counting
+console.count('Wes');
+console.count('Steve');
+console.count('Max');
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Max');
+console.count('Wes');
+
+//Timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
